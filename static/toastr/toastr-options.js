6fc70c17958e2970e7ef4829dd223837210d4df3
Andersson Torres <anderssontorrresr@gmail.com>
2022-11-01 22:33:46 -0500

    Cambios funcionales

---
+++ static/toastr/toastr-options.js
@@ -0,0 +1,16 @@
+//https://codeseven.github.io/toastr/demo.html
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-bottom-left",
+    "preventDuplicates": false,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"}
